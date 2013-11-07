c086c6f32a9a839ede00f0062fd03e7aa1e444d6
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2013-11-07 14:13:39 +0000

    Use actual extensions

--- device.go
+++ device.go
@@ -4,26 +4,26 @@ package openrtb
 // platform, location, and carrier. This device can refer to a mobile handset, a desktop computer,
 // set top box or other digital device.
 type Device struct {
-	Dnt            *int              `json:"dnt,omitempty"` // "1": Do not track
-	Ua             *string           `json:"ua,omitempty"`  // User agent
-	Ip             *string           `json:"ip,omitempty"`  // IPv4
-	Geo            *Geo              `json:"geo,omitempty"`
-	Didsha1        *string           `json:"didsha1,omitempty"`  // SHA1 hashed device ID
-	Didmd5         *string           `json:"didmd5,omitempty"`   // MD5 hashed device ID
-	Dpidsha1       *string           `json:"dpidsha1,omitempty"` // SHA1 hashed platform device ID
-	Dpidmd5        *string           `json:"dpidmd5,omitempty"`  // MD5 hashed platform device ID
-	Ipv6           *string           `json:"ipv6,omitempty"`     // IPv6
-	Carrier        *string           `json:"carrier,omitempty"`  // Carrier or ISP derived from the IP address
-	Language       *string           `json:"language,omitempty"` // Browser language
-	Make           *string           `json:"make,omitempty"`     // Device make
-	Model          *string           `json:"model,omitempty"`    // Device model
-	Os             *string           `json:"os,omitempty"`       // Device OS
-	Osv            *string           `json:"osv,omitempty"`      // Device OS version
-	Js             *int              `json:"js,omitempty"`       // Javascript status ("0": Disabled, "1": Enabled)
-	Connectiontype *int              `json:"connectiontype,omitempty"`
-	Devicetype     *int              `json:"devicetype,omitempty"`
-	Flashver       *string           `json:"flashver,omitempty"` // Flash version
-	Ext            map[string]string `json:"ext,omitempty"`
+	Dnt            *int       `json:"dnt,omitempty"` // "1": Do not track
+	Ua             *string    `json:"ua,omitempty"`  // User agent
+	Ip             *string    `json:"ip,omitempty"`  // IPv4
+	Geo            *Geo       `json:"geo,omitempty"`
+	Didsha1        *string    `json:"didsha1,omitempty"`  // SHA1 hashed device ID
+	Didmd5         *string    `json:"didmd5,omitempty"`   // MD5 hashed device ID
+	Dpidsha1       *string    `json:"dpidsha1,omitempty"` // SHA1 hashed platform device ID
+	Dpidmd5        *string    `json:"dpidmd5,omitempty"`  // MD5 hashed platform device ID
+	Ipv6           *string    `json:"ipv6,omitempty"`     // IPv6
+	Carrier        *string    `json:"carrier,omitempty"`  // Carrier or ISP derived from the IP address
+	Language       *string    `json:"language,omitempty"` // Browser language
+	Make           *string    `json:"make,omitempty"`     // Device make
+	Model          *string    `json:"model,omitempty"`    // Device model
+	Os             *string    `json:"os,omitempty"`       // Device OS
+	Osv            *string    `json:"osv,omitempty"`      // Device OS version
+	Js             *int       `json:"js,omitempty"`       // Javascript status ("0": Disabled, "1": Enabled)
+	Connectiontype *int       `json:"connectiontype,omitempty"`
+	Devicetype     *int       `json:"devicetype,omitempty"`
+	Flashver       *string    `json:"flashver,omitempty"` // Flash version
+	Ext            Extensions `json:"ext,omitempty"`
 }
 
 // Returns the DNT status, with default fallback
