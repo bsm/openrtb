a2296072cb03fed74b314186595e637d37553488
Dhaivat Pandit <me@dhaiv.at>
2015-01-29 18:01:00 -0800

    Added ParseResponse and ParseResponseBytes methods
        * added test coverage as well

--- response_test.go
+++ response_test.go
@@ -1,12 +1,15 @@
 package openrtb
 
 import (
+	"bytes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Response", func() {
 	var subject *Response
+	var sptr = func(s string) *string { return &s }
+	var fptr = func(f float32) *float32 { return &f }
 
 	BeforeEach(func() {
 		subject = new(Response)
@@ -27,5 +30,25 @@ var _ = Describe("Response", func() {
 		ok, err = subject.Valid()
 		Expect(err).NotTo(HaveOccurred())
 		Expect(ok).To(BeTrue())
+	})
+
+	It("should parse responses", func() {
+		resp, err := ParseResponse(bytes.NewBuffer(testFixtures.simpleResponse))
+		Expect(err).NotTo(HaveOccurred())
+
+		bid := Bid{
+			Id:      sptr("32a69c6ba388f110487f9d1e63f77b22d86e916b"),
+			Impid:   sptr("32a69c6ba388f110487f9d1e63f77b22d86e916b"),
+			Price:   fptr(0.065445),
+			Adid:    sptr("529833ce55314b19e8796116"),
+			Nurl:    sptr("http://ads.com/win/529833ce55314b19e8796116?won=${auction_price}"),
+			Adm:     sptr("<iframe src=\"foo.bar\"/>"),
+			Adomain: []string{},
+			Attr:    []int{},
+			Cid:     sptr("529833ce55314b19e8796116"),
+			Crid:    sptr("529833ce55314b19e8796116_1385706446"),
+		}
+
+		Expect(resp.Seatbid[0].Bid[0]).To(Equal(bid))
 	})
 })
