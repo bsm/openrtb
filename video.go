524f629783ebf3975b3196d80588c6b8a6f23f24
Ian Davis <nospam@iandavis.com>
2015-03-25 19:37:56 +0000

    video: changed protocol field to interface{}
    one non-conforming exchange sends multiple protocols in both the protocols and the protocol field which causes an unmarshalling error

--- video.go
+++ video.go
@@ -7,28 +7,28 @@ import (
 // The "video" object must be included directly in the impression object if the impression offered
 // for auction is an in-stream video ad opportunity.
 type Video struct {
-	Mimes          []string   `json:"mimes,omitempty"`          // Content MIME types supported.
-	Minduration    *int       `json:"minduration,omitempty"`    // Minimum video ad duration in seconds
-	Maxduration    *int       `json:"maxduration,omitempty"`    // Maximum video ad duration in seconds
-	Protocol       *int       `json:"protocol,omitempty"`       // Video bid response protocols
-	Protocols      []int      `json:"protocols,omitempty"`      // Video bid response protocols
-	W              *int       `json:"w,omitempty"`              // Width of the player in pixels
-	H              *int       `json:"h,omitempty"`              // Height of the player in pixels
-	Startdelay     *int       `json:"startdelay,omitempty"`     // Indicates the start delay in seconds
-	Linearity      *int       `json:"linearity,omitempty"`      // Indicates whether the ad impression is linear or non-linear
-	Sequence       *int       `json:"sequence,omitempty"`       // Default: 1
-	Battr          []int      `json:"battr,omitempty"`          // Blocked creative attributes
-	Maxextended    *int       `json:"maxextended,omitempty"`    // Maximum extended video ad duration
-	Minbitrate     *int       `json:"minbitrate,omitempty"`     // Minimum bit rate in Kbps
-	Maxbitrate     *int       `json:"maxbitrate,omitempty"`     // Maximum bit rate in Kbps
-	Boxingallowed  *int       `json:"boxingallowed,omitempty"`  // If exchange publisher has rules preventing letter boxing
-	Playbackmethod []int      `json:"playbackmethod,omitempty"` // List of allowed playback methods
-	Delivery       []int      `json:"delivery,omitempty"`       // List of supported delivery methods
-	Pos            *int       `json:"pos,omitempty"`            // Ad Position
-	Companionad    []Banner   `json:"companionad,omitempty"`
-	Api            []int      `json:"api,omitempty"` // List of supported API frameworks
-	Companiontype  []int      `json:"companiontype,omitempty"`
-	Ext            Extensions `json:"ext,omitempty"`
+	Mimes          []string    `json:"mimes,omitempty"`          // Content MIME types supported.
+	Minduration    *int        `json:"minduration,omitempty"`    // Minimum video ad duration in seconds
+	Maxduration    *int        `json:"maxduration,omitempty"`    // Maximum video ad duration in seconds
+	Protocol       interface{} `json:"protocol,omitempty"`       // Video bid response protocols - use interface{} to cater for nonconformant exchanges
+	Protocols      []int       `json:"protocols,omitempty"`      // Video bid response protocols
+	W              *int        `json:"w,omitempty"`              // Width of the player in pixels
+	H              *int        `json:"h,omitempty"`              // Height of the player in pixels
+	Startdelay     *int        `json:"startdelay,omitempty"`     // Indicates the start delay in seconds
+	Linearity      *int        `json:"linearity,omitempty"`      // Indicates whether the ad impression is linear or non-linear
+	Sequence       *int        `json:"sequence,omitempty"`       // Default: 1
+	Battr          []int       `json:"battr,omitempty"`          // Blocked creative attributes
+	Maxextended    *int        `json:"maxextended,omitempty"`    // Maximum extended video ad duration
+	Minbitrate     *int        `json:"minbitrate,omitempty"`     // Minimum bit rate in Kbps
+	Maxbitrate     *int        `json:"maxbitrate,omitempty"`     // Maximum bit rate in Kbps
+	Boxingallowed  *int        `json:"boxingallowed,omitempty"`  // If exchange publisher has rules preventing letter boxing
+	Playbackmethod []int       `json:"playbackmethod,omitempty"` // List of allowed playback methods
+	Delivery       []int       `json:"delivery,omitempty"`       // List of supported delivery methods
+	Pos            *int        `json:"pos,omitempty"`            // Ad Position
+	Companionad    []Banner    `json:"companionad,omitempty"`
+	Api            []int       `json:"api,omitempty"` // List of supported API frameworks
+	Companiontype  []int       `json:"companiontype,omitempty"`
+	Ext            Extensions  `json:"ext,omitempty"`
 }
 
 // Returns the sequence number, with default fallback
@@ -126,9 +126,6 @@ func (v *Video) SetMaxduration(mdur int) *Video {
 
 // Set the Protocol
 func (v *Video) SetProtocol(p int) *Video {
-	if v.Protocol == nil {
-		v.Protocol = new(int)
-	}
-	*v.Protocol = p
+	v.Protocol = p
 	return v
 }
