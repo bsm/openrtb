883f34c14c06e10145bcf05e0a0a8f7aa182e2fe
dsociative <artem.prikhodin@iage.net>
2016-02-29 17:36:32 +0300

    Ext json.RawMessage > Ext *json.RawMessage

--- banner.go
+++ banner.go
@@ -9,19 +9,19 @@ import "encoding/json"
 // VAST response to dictate placement of the companion creatives when multiple companion ad
 // opportunities of the same size are available on a page.
 type Banner struct {
-	W        int             `json:"w,omitempty"`        // Width
-	H        int             `json:"h,omitempty"`        // Height
-	WMax     int             `json:"wmax,omitempty"`     // Width maximum
-	HMax     int             `json:"hmax,omitempty"`     // Height maximum
-	WMin     int             `json:"wmin,omitempty"`     // Width minimum
-	HMin     int             `json:"hmin,omitempty"`     // Height minimum
-	ID       string          `json:"id,omitempty"`       // A unique identifier
-	Pos      int             `json:"pos,omitempty"`      // Ad Position
-	BType    []int           `json:"btype,omitempty"`    // Blocked creative types
-	BAttr    []int           `json:"battr,omitempty"`    // Blocked creative attributes
-	Mimes    []string        `json:"mimes,omitempty"`    // Whitelist of content MIME types supported
-	TopFrame int             `json:"topframe,omitempty"` // Default: 0 ("1": Delivered in top frame, "0": Elsewhere)
-	ExpDir   []int           `json:"expdir,omitempty"`   // Specify properties for an expandable ad
-	Api      []int           `json:"api,omitempty"`      // List of supported API frameworks
-	Ext      json.RawMessage `json:"ext,omitempty"`
+	W        int              `json:"w,omitempty"`        // Width
+	H        int              `json:"h,omitempty"`        // Height
+	WMax     int              `json:"wmax,omitempty"`     // Width maximum
+	HMax     int              `json:"hmax,omitempty"`     // Height maximum
+	WMin     int              `json:"wmin,omitempty"`     // Width minimum
+	HMin     int              `json:"hmin,omitempty"`     // Height minimum
+	ID       string           `json:"id,omitempty"`       // A unique identifier
+	Pos      int              `json:"pos,omitempty"`      // Ad Position
+	BType    []int            `json:"btype,omitempty"`    // Blocked creative types
+	BAttr    []int            `json:"battr,omitempty"`    // Blocked creative attributes
+	Mimes    []string         `json:"mimes,omitempty"`    // Whitelist of content MIME types supported
+	TopFrame int              `json:"topframe,omitempty"` // Default: 0 ("1": Delivered in top frame, "0": Elsewhere)
+	ExpDir   []int            `json:"expdir,omitempty"`   // Specify properties for an expandable ad
+	Api      []int            `json:"api,omitempty"`      // List of supported API frameworks
+	Ext      *json.RawMessage `json:"ext,omitempty"`
 }
