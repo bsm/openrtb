53fa0f42fb7d88aa45113c43069b73bd68ac173c
Art <admin@geektech.ru>
2016-09-23 13:34:02 +0300

    ImpressionExt added

--- impression.go
+++ impression.go
@@ -13,26 +13,30 @@ var (
 	ErrInvalidImpMultiAssets = errors.New("openrtb: impression has multiple assets") // at least two out of Banner, Video, Native
 )
 
+type ImpressionExt struct {
+	BillingID []string `json:"billing_id"`
+}
+
 // The "imp" object describes the ad position or impression being auctioned.  A single bid request
 // can include multiple "imp" objects, a use case for which might be an exchange that supports
 // selling all ad positions on a given page as a bundle.  Each "imp" object has a required ID so that
 // bids can reference them individually.  An exchange can also conduct private auctions by
 // restricting involvement to specific subsets of seats within bidders.
 type Impression struct {
-	ID                string           `json:"id"` // A unique identifier for this impression
-	Banner            *Banner          `json:"banner,omitempty"`
-	Video             *Video           `json:"video,omitempty"`
-	Native            *Native          `json:"native,omitempty"`
-	DisplayManager    string           `json:"displaymanager,omitempty"`    // Name of ad mediation partner, SDK technology, etc
-	DisplayManagerVer string           `json:"displaymanagerver,omitempty"` // Version of the above
-	Instl             int              `json:"instl,omitempty"`             // Interstitial, Default: 0 ("1": Interstitial, "0": Something else)
-	TagID             string           `json:"tagid,omitempty"`             // IDentifier for specific ad placement or ad tag
-	BidFloor          float64          `json:"bidfloor,omitempty"`          // Bid floor for this impression in CPM
-	BidFloorCurrency  string           `json:"bidfloorcur,omitempty"`       // Currency of bid floor
-	Secure            int              `json:"secure,omitempty"`            // Flag to indicate whether the impression requires secure HTTPS URL creative assets and markup.
-	IFrameBuster      []string         `json:"iframebuster,omitempty"`      // Array of names for supportediframe busters.
-	Pmp               *Pmp             `json:"pmp,omitempty"`               // A reference to the PMP object containing any Deals eligible for the impression object.
-	Ext               *json.RawMessage `json:"ext,omitempty"`
+	ID                string        `json:"id"` // A unique identifier for this impression
+	Banner            *Banner       `json:"banner,omitempty"`
+	Video             *Video        `json:"video,omitempty"`
+	Native            *Native       `json:"native,omitempty"`
+	DisplayManager    string        `json:"displaymanager,omitempty"`    // Name of ad mediation partner, SDK technology, etc
+	DisplayManagerVer string        `json:"displaymanagerver,omitempty"` // Version of the above
+	Instl             int           `json:"instl,omitempty"`             // Interstitial, Default: 0 ("1": Interstitial, "0": Something else)
+	TagID             string        `json:"tagid,omitempty"`             // IDentifier for specific ad placement or ad tag
+	BidFloor          float64       `json:"bidfloor,omitempty"`          // Bid floor for this impression in CPM
+	BidFloorCurrency  string        `json:"bidfloorcur,omitempty"`       // Currency of bid floor
+	Secure            int           `json:"secure,omitempty"`            // Flag to indicate whether the impression requires secure HTTPS URL creative assets and markup.
+	IFrameBuster      []string      `json:"iframebuster,omitempty"`      // Array of names for supportediframe busters.
+	Pmp               *Pmp          `json:"pmp,omitempty"`               // A reference to the PMP object containing any Deals eligible for the impression object.
+	Ext               ImpressionExt `json:"ext,omitempty"`
 }
 
 func (imp *Impression) assetCount() int {
