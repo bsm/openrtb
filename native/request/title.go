aa04a26112e5bb15becb610721591aae611ff97c
Nabib El-Rahman <nabib@unity3d.com>
2017-06-30 13:49:22 -0700

    Forked bsm/openrtb project to not enforce min/max duration

--- native/request/title.go
+++ native/request/title.go
@@ -1,6 +1,6 @@
 package request
 
-import "github.com/bsm/openrtb"
+import "github.com/UnityTech/openrtb"
 
 type Title struct {
 	Length int               `json:"len"` // Maximum length of the text in the title element
--- native/response/image.go
+++ native/response/image.go
@@ -1,6 +1,6 @@
 package response
 
-import "github.com/bsm/openrtb"
+import "github.com/UnityTech/openrtb"
 
 type Image struct {
 	URL    string            `json:"url,omitempty"` // URL of the image asset
